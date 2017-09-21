78782363331e9f281f2fc106461d1668bba0d7ca
Damilola Payne <DamiPayne@users.noreply.github.com>
2017-09-21 10:21:16 +0100

    Update VirtualBox_Screen_Resolution_Fix.bat

--- VirtualBox_Screen_Resolution_Fix.bat
+++ VirtualBox_Screen_Resolution_Fix.bat
@@ -3,4 +3,4 @@ cd "C:\Program Files\Oracle\Virtualbox"
 VBoxManage setextradata "VM Name" VBoxInternal2/EfiGopMode N
 
 rem Where N can be one of 0,1,2,3,4,5 referring to the 640x480, 800x600, 1024x768, 1280x1024, 1440x900, 1900x1200
-rem                            0	       1	      2	          3	      4 	        5
+rem                                   0	   1	      2            3	       4 	     5

d2ba95fcd7ffd8152af4a0b5b5fc6f0ca6d5e84a
Damilola Payne <DamiPayne@users.noreply.github.com>
2017-09-21 10:23:44 +0100

    Update VirtualBox_Screen_Resolution_Fix.bat

--- VirtualBox_Screen_Resolution_Fix.bat
+++ VirtualBox_Screen_Resolution_Fix.bat
@@ -1,5 +1,5 @@
 cd "C:\Program Files\Oracle\Virtualbox"
 
-VBoxManage setextradata "VM Name" VBoxInternal2/EfiGopMode N
+VBoxManage setextradata "macOS Sierra" VBoxInternal2/EfiGopMode N
 
 rem Where N can be one of 0,1,2,3,4,5 referring to the 640x480, 800x600, 1024x768, 1280x1024, 1440x900, 1900x1200 respectively
